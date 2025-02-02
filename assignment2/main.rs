1b3143e2acd0420a46aacf6922fe0ffff6c222d1
Magnus <91549385+KoksILasten@users.noreply.github.com>
2025-02-02 19:20:12 +0100

    den plottar nu men supportar inte interaktivitet

--- assignment2/main.rs
+++ assignment2/main.rs
@@ -1,4 +1,4 @@
-use plotters::prelude::*;
+use plotters::{prelude::*, style::full_palette::GREY_A100};
 use serde::{Deserialize};
 
 
@@ -26,7 +26,7 @@ fn load_csv(filename: &str) -> Vec<Data> {
     for result in reader.deserialize() {
 
         let record: Data = result.unwrap();
-        println!("{:?}", record);
+        //println!("{:?}", record);
         data.push(record);
        
     }
@@ -39,31 +39,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let data = load_csv("data2.csv");
 
+    //super non-effective lmao
+    let max_x_val = data.iter().map(|x| x.x).fold(0./0., f64::max);
+    let max_y_val = data.iter().map(|x| x.y).fold(0./0., f64::max);
+    let min_x_val = data.iter().map(|x| x.x).fold(0./0., f64::min);
+    let min_y_val = data.iter().map(|x| x.y).fold(0./0., f64::min);
 
-    let root = BitMapBackend::new("plotters-doc-data/0.png", (640, 480)).into_drawing_area();
-    root.fill(&WHITE)?;
+    //draw background
+    let root = BitMapBackend::new("plotters-doc-data/0.png", (640*2, 480*2)).into_drawing_area();
+    root.fill(&GREY_A100)?;
+
+    //create a chart with max and min values
     let mut chart = ChartBuilder::on(&root)
-        .caption("y=x^2", ("sans-serif", 50).into_font())
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+    .caption("scatter plot", ("sans-serif", 50).into_font())
+    .margin( 5)
+    .x_label_area_size(30)
+    .y_label_area_size(30)
+    .build_cartesian_2d((min_x_val - 5.0)..(max_x_val + 5.0), (min_y_val - 5.0)..(max_y_val + 5.0))?;
 
     chart.configure_mesh().draw()?;
 
-    chart
-        .draw_series(LineSeries::new(
-            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-            &RED,
-        ))?
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    //koden är stekt sori :(
+    let dot_and_label = |x: f64, y: f64, name:String | {
+        if name == "a" || name == "foo" {
+            return EmptyElement::at((x, y))
+            + Circle::new((0, 0), 3, ShapeStyle::from(&RED).filled())
+            // + Text::new(
+            //     format!("{:}:({:.2},{:.2})", name, x, y),
+            //     (10, 0),
+            //     ("sans-serif", 15.0).into_font(),
+            // );
+        }
+        else if name == "b" || name == "baz" {
+            return EmptyElement::at((x, y))
+            + Circle::new((0, 0), 3, ShapeStyle::from(&BLUE).filled())
+            // + Text::new(
+            //     format!("{:}:({:.2},{:.2})", name, x, y),
+            //     (10, 0),
+            //     ("sans-serif", 15.0).into_font(),
+            // );
+        }
+        else {
+            return EmptyElement::at((x, y))
+            + Circle::new((0, 0), 3, ShapeStyle::from(&YELLOW).filled());
+            // + Text::new(
+            //     format!("{:}:({:.2},{:.2})", name, x, y),
+            //     (10, 0),
+            //     ("sans-serif", 15.0).into_font(),
+            // );
+        }
+    };
 
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
+    chart.draw_series(data.iter().map(|p| dot_and_label(p.x, p.y, p.name.clone())))?;
 
     root.present()?;
 
