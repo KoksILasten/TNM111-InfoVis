5dfa2f0bb9d26be18e2ebbf18d228f5062d09d21
Magnus <91549385+KoksILasten@users.noreply.github.com>
2025-02-02 17:08:54 +0100

    Finns nu en fungerande csv loader

--- assignment2/main.rs
+++ assignment2/main.rs
@@ -1,7 +1,45 @@
-
 use plotters::prelude::*;
+use serde::{Deserialize};
+
+
+#[derive(Debug, Deserialize)]
+struct Data {
+    x: f64,
+    y: f64,
+    name: String,
+}
+
+fn load_csv(filename: &str) -> Vec<Data> {
+
+    //let mut rdr = csv::Reader::from_path(filename).unwrap();
+
+    //create a reader for the csv files
+    let mut reader = csv::ReaderBuilder::new()
+    .has_headers(false)
+    .from_path(filename)
+    .unwrap();
+
+    //vector for the data using struct Data
+    let mut data: Vec<Data> = Vec::new();
+
+    //iterate through the csv file
+    for result in reader.deserialize() {
+
+        let record: Data = result.unwrap();
+        println!("{:?}", record);
+        data.push(record);
+       
+    }
+    return data;
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    println!("Creating a plot");
+
+    let data = load_csv("data2.csv");
+
+
     let root = BitMapBackend::new("plotters-doc-data/0.png", (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
     let mut chart = ChartBuilder::on(&root)
@@ -29,5 +67,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     root.present()?;
 
+    println!("Plot created");
     Ok(())
 }
