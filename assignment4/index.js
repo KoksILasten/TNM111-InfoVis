423682a6d2e0ca39e84be05a3fcc75a4e8cc72af
Magnus <91549385+KoksILasten@users.noreply.github.com>
2025-02-23 17:14:55 +0100

    fixade d3 json load

--- assignment4/index.js
+++ assignment4/index.js
@@ -1,19 +1,20 @@
+var swep1, swep2, swep3, swep4, swep5, swep6, swep7, swfull;
 
-window.onload = function() {
-    loadJSON();
+window.onload = async function() {
+    await loadJSON();
+
+    console.log("Loaded Data:");
+    console.log("Episode 1:", swep1);
+    console.log("Episode 2:", swep2);
+    console.log("Episode 3:", swep3);
+    console.log("Episode 4:", swep4);
+    console.log("Episode 5:", swep5);
+    console.log("Episode 6:", swep6);
+    console.log("Episode 7:", swep7);
+    console.log("Full:", swfull);
 };
 
-function getJSONFile(path, callback) {
-    
-    fetch(path)
-        .then(response => response.json())
-        .then(data => callback(data))
-        .catch(error => console.error('Error fetching JSON:', error));
-}
-
-
-function loadJSON(){
-
+async function loadJSON() {
     let links = [
         "./data/starwars-episode-1-interactions-allCharacters.json",
         "./data/starwars-episode-2-interactions-allCharacters.json",
@@ -25,12 +26,15 @@ function loadJSON(){
         "./data/starwars-full-interactions-allCharacters.json"
     ];
 
-    links.forEach(link => {
-        getJSONFile(link, data => {
-            console.log(data);
-        });
-    });
-
-
+    try {
+        const dataArray = await Promise.all(links.map(link => d3.json(link)));
+        [swep1, swep2, swep3, swep4, swep5, swep6, swep7, swfull] = dataArray;
+    } catch (error) {
+        console.error('Error loading JSON files:', error);
+    }
 }
+
+
+
+
 
